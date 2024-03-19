cc94dc1959149099dbbe931bbc26a1f280ce4034
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-03-19 11:27:39 +0100

    lien de film preview vers page film fonctionnel

---
+++ src/components/filmPreview.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Film } from "@/types";
+import { ref } from "vue";
+import { supabase } from "@/supabase";
+
+const props = defineProps<{
+    data?: Film;
+    id?: string;
+}>();
+
+const films = ref<Film>(props.data ?? {});
+
+if (props.id !== undefined) {
+    const { data, error } = await supabase.from("Film").select("*").eq("id", props.id).single();
+    if (error) console.error("error", error);
+    else films.value = data;
+}
+</script>
+
+
+<template>
+    <p>nom du film prev</p>
+    <h1 class="text-2xl">{{ films.nom_original }}</h1>
+</template>
--- src/pages/film/index.vue
+++ src/pages/film/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import filmPreview from "@/components/filmPreview.vue";
 import { supabase } from "@/supabase";
 console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
 const { data: Film, error } = await supabase
@@ -11,4 +12,13 @@ const { data: Film, error } = await supabase
 
 <template>
     <h1>Liste de tous les films</h1>
+
+    <div class="flex flex-wrap gap-2">
+            <RouterLink class="w-64" v-for="film in Film" :key="film.id" :to="{
+                name: '/film/[id]',
+                params: { id: film.id },
+            }">
+                <filmPreview />
+            </RouterLink>
+        </div>
 </template>
