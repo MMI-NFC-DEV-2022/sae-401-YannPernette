2bcde9c2aecac7386341cb1318bf8382928a9227
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-03-20 11:43:26 +0100

    style header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import { supabase, user } from '@/supabase';
+import headerComposant from './components/headerComposant.vue';
 </script>
 
 <template>
-  <header>
-    <nav class="flex gap-10 jushover:bg-white hover:text-gray-600 border-solid border-2 py-2 px-4 h-12">
-      Bonjour je suis un header
-    </nav>
-  </header>
 
+  <headerComposant />
 
   <Suspense>
     <RouterView />
-
   </Suspense>
 
 
---
+++ src/components/headerComposant.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import logo from './icon/logo.vue';
+</script>
+
+<template>
+    <header>
+        <nav
+            class="fixed z-50 bgHeader text-white flex items-center justify-between w-full px-5 py-2 uppercase font-light tracking-wider">
+
+            <RouterLink to="/">
+                <logo class="w-16" color="white" />
+            </RouterLink>
+
+            <ul class="flex text-lg gap-10">
+                <li>
+                    <RouterLink to="/film">Films</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/celebrite">Célébrités</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/">Collections</RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/">Genres</RouterLink>
+                </li>
+            </ul>
+
+            <ul class="flex uppercase font-poppins gap-3">
+                <li class="bg-white text-black rounded-sm px-2 py-1">
+                    <RouterLink to="/">S'inscrire</RouterLink>
+                </li>
+                <li class="border rounded-sm px-2 py-1">
+                    <RouterLink to="/">Se connecter</RouterLink>
+                </li>
+            </ul>
+
+        </nav>
+    </header>
+</template>
