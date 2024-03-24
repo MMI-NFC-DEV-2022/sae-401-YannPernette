742f3d6710b4f545a526e5c6e09064de53a288ff
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-03-24 20:26:55 +0100

    amélioration page accueil (ajout setion 3 premiers films)

--- src/components/accueilComposant.vue
+++ src/components/accueilComposant.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import Logo from '../components/icon/Logo.vue';
 import ArrowScroll from '../components/icon/ArrowScroll.vue';
+
+import filmPreview from "@/components/filmPreview.vue";
+import { supabase } from "@/supabase";
+console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+const { data: Film, error } = await supabase
+    .from('Film')
+    .select('*')
+    .limit(3);
 </script>
 
 <template>
@@ -8,16 +16,41 @@ import ArrowScroll from '../components/icon/ArrowScroll.vue';
         <img class="h-screen w-full object-cover" src="/images/movie_theatre.webp" alt="Salle de cinéma - Accueil">
 
         <div class="absolute top-[18%] left-1/2 transform -translate-x-1/2 flex items-center gap-10">
-                <Logo class="size-36" color="white" />
-                <h1 class="text-white top-20 font-sora text-7xl font-semibold">MovieSpotter</h1>
+            <Logo class="size-36" color="white" />
+            <h1 class="text-white top-20 font-sora text-7xl font-semibold">MovieSpotter</h1>
         </div>
 
         <div class="absolute top-[40%] left-1/2 transform -translate-x-1/2">
-            <p class="text-white text-xl font-semibold uppercase shadow-2xl text-center">Tous vos films préférés en un seul endroit</p>
+            <p class="text-white text-xl font-semibold uppercase shadow-2xl text-center">Tous vos films préférés en un
+                seul endroit</p>
         </div>
 
         <div class="absolute bottom-[5%] left-1/2 transform -translate-x-1/2">
-            <ArrowScroll class="size-16 slideUp" />
+            <a href="#Accueil">
+                <ArrowScroll class="size-16 slideUp" />
+            </a>
         </div>
+    </div>
+
+
+    <div class="mx-[10%] mb-20">
+        <h1 id="Accueil" class="pt-20 pb-14 text-3xl font-sora font-bold uppercase tracking-wider">Sélection des films
+            du moment</h1>
+
+        <div class="flex flex-wrap gap-20 mb-6">
+            <RouterLink class="w-64" v-for="film in Film" :key="film.id" :to="{
+                name: '/film/[id]',
+                params: { id: film.id },
+            }">
+                <filmPreview v-bind="{ ...film, id: film.id.toString() }" />
+            </RouterLink>
+        </div>
+
+        <RouterLink to="/film">
+            <div
+                class="bg-[#FFCB47] hover:bg-black hover:text-[#FFCB47] transition-colors duration-300 rounded-md px-5 py-2 w-fit">
+                <p class="text-2xl">Voir plus de films</p>
+            </div>
+        </RouterLink>
     </div>
 </template>
